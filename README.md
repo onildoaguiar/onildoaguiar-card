549be98717c72f1f6778706419dc36439c19e684
Onildo Aguiar <onildo.aguiar@live.com>
2019-06-04 11:34:12 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Onildo Aguiar's Card
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/onildoaguiar/onildoaguiar-card/blob/master/LICENSE) 
+[![npm version](https://badge.fury.io/js/%40onildoaguiar%2Fcard.svg)](https://www.npmjs.com/package/@onildoaguiar/card)
+
+## Usage
+
+Via npx:
+
+```bash
+npx @onildoaguiar/card
+```
+
+Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
+
+## Credits
+
+Tierney Cyren's https://github.com/bnb/bitandbang <br>
+Badges from <https://badge.fury.io/for/js>
+
+# License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
---
+++ bin/card.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const boxen = require('boxen');
+const chalk = require('chalk');
+
+const logger = console;
+
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'single',
+};
+
+const data = {
+  name: chalk.white('Onildo Aguiar /'),
+  handle: chalk.blueBright('Drizzle Land Studios'),
+  work: chalk.white('Full Stack Developer'),
+  twitter: chalk.blueBright('https://twitter.com/onildoaguiar'),
+  github: chalk.blueBright('https://github.com/onildoaguiar'),
+  npm: chalk.blueBright('https://www.npmjs.com/~onildoaguiar'),
+  linkedin: chalk.blueBright('https://linkedin.com/in/onildoaguiar'),
+  web: chalk.blueBright('https://onildoaguiar.com'),
+  npx: chalk.white('npx @onildoaguiar/card'),
+  labelWork: chalk.white.bold('      Work:'),
+  labelTwitter: chalk.white.bold('   Twitter:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelNpm: chalk.white.bold('       NPM:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelWeb: chalk.white.bold('       Web:'),
+  labelCard: chalk.white.bold('      Card:'),
+};
+
+const newline = '\n';
+const heading = `${data.name} ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const npming = `${data.labelNpm}  ${data.npm}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
+
+const output = heading
+  + newline
+  + newline
+  + working
+  + newline
+  + twittering
+  + newline
+  + githubing
+  + newline
+  + npming
+  + newline
+  + linkedining
+  + newline
+  + webing
+  + newline
+  + newline
+  + carding;
+
+const cardMessage = chalk.yellow(boxen(output, options));
+logger.log(cardMessage);
